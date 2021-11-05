6b1a2d8039425fd2ec9e7b76b704b0ecb444b9e3
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-11-05 11:25:15 +0000

    working button to change title

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("helloTher")
+// const createTable = function () {
+//     getRow = document.getElementsByTagName('table')
+//     let tableHeading = createElement("th")
+//     tableHeading.innerText = "hello"
+//     getRow.appendChild(tableHeading)
+// }
+// createTable()
+
+const textButton = function(eventData){
+    let buttonTextChanger = document.getElementById('text-change-button')
+    buttonTextChanger.addEventListener = ('onclick',textChanger())
+}
+
+const textChanger = function(){
+    let getText = document.getElementById('text-change')
+    let heading = document.querySelector("h1")
+    heading.innerText = getText.value
+}
